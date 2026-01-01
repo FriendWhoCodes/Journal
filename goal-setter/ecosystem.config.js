2cb8b40a485574a8192e6f2aeb3f4047e95e6f51
FriendWhoCodes <hunkwhocodes@gmail.com>
2026-01-01 07:55:05 +0000

    Revert "Fix ecosystem.config.js path for production deployment"
    This reverts commit f1379bb98107819cb2f8fe3b3b88c451a4803261.

--- goal-setter/ecosystem.config.js
+++ goal-setter/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'goal-setter',
     script: 'npm',
     args: 'start',
-    cwd: '/var/www/Journal/goal-setter',
+    cwd: '/root/var/www/goal-setter/goal-setter',
     instances: 1,
     exec_mode: 'fork',
     env: {
